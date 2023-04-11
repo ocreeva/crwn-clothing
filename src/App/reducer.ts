d4074bafbb5e5b53822005b2202dfee720ad7aaa
Brian Perrin <perrin@live.com>
2023-04-11 09:45:25 -0700

    Convert remaining store-related components to typescript.

--- src/App/reducer.ts
+++ src/App/reducer.ts
@@ -2,11 +2,11 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import cartReducer from "../features/cart";
-import categoriesReducer from "../features/categories";
-import dropdownReducer from "../features/dropdown";
-import productsReducer from "../features/products";
-import userReducer from "../features/user";
+import cartReducer from "features/cart";
+import categoriesReducer from "features/categories";
+import dropdownReducer from "features/dropdown";
+import productsReducer from "features/products";
+import userReducer from "features/user";
 
 const persistConfig = {
     key: "root",
